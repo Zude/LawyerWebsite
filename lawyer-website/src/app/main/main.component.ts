c92a3e429ea7c47e906f9cb1bb517f7b347717eb
Alexander Löffler <alex@Alexanders-Air.fritz.box>
2018-12-28 18:49:32 +0100

    Header and first Stuff

---
+++ lawyer-website/src/app/main/main.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css']
+})
+export class MainComponent implements OnInit {
+
+  name = 'Klaus Löffler';
+  job = 'Rechtsanwalt';
+  subHead = 'Ich bin der Anwalt deines Vertrauens';
+  introduction = 'Hey ich bin ein cooler Anwalt für cooles Zeug!!!';
+  services = 'Ich mache dies und das außerdem dings';
+  aboutme = 'ich bin ein geiler boi und habe viel erfahrung';
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
