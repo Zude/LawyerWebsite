bf5bc668801e554241a485224a546a79e7d4a6d6
Alexander Löffler <alex@Alexanders-Air.fritz.box>
2019-01-03 13:56:23 +0100

    design changed

--- lawyer-website/src/app/main/main.component.ts
+++ lawyer-website/src/app/main/main.component.ts
@@ -11,7 +11,7 @@ export class MainComponent implements OnInit {
   job = 'Rechtsanwalt';
   subHead = 'Ich bin der Anwalt deines Vertrauens';
   introduction = 'Willkommen auf meiner Homepage';
-  intro1 = 'Hier finden sie einen richtig guten Text, der sie sofort von dieser atemberaubenden Homepage und mir als anwalt total überzeugt. ja ich bin ein super coller typ';
+  intro1 = 'Hier finden sie einen richtig guten Text, der sie sofort von dieser atemberaubenden Homepage und mir als anwalt total überzeugt. j Hier finden sie einen richtig guten Text, der sie sofort von dieser atemberaubenden Homepage und mir als anwalt total überzeugt. ja ich bin ein super coller typa ich bin ein super coller typ';
 
   services = 'Leistungen';
   serv1 = 'Strafrecht';
@@ -25,7 +25,11 @@ export class MainComponent implements OnInit {
   serv4text = 'das thema ist super wichtig und ich bin voll gut daron';
   serv5text = 'das thema ist super wichtig und ich bin voll gut daron';
   
-  aboutme = 'ich bin ein geiler boi und habe viel erfahrung';
+  aboutme = 'Über mich';
+  aboutme1 = 'hdufgbnaougosahgfhsoifhioahfioebfhajskdhfkshfjd dddsdsd jkjlkajslkdjalksdjadsjlakljdakldjaklsdjladdhfjksguobuigbiuafbesuof';
+  aboutme2 = 'hdufgbnaougosahgfhsoifhioahfioebguobuigbiuafbesuof';
+  aboutme3 = 'hdufgbnaougosahgfhsoifhioahfioebguobuigbiuafbesuof';
+  aboutm4 = 'hdufgbnaougosahgfhsoifhioahfioebguobuigbiuafbesuof';
 
   scroll(el) {
     el.scrollIntoView({behavior: "smooth"});
