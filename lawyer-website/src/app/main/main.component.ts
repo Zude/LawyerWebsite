462f9f92e6cde5ee555d482ae871cbf4c122529d
Zude <Alex.93.loeffler@me.com>
2019-04-01 14:27:36 +0200

    Version 1.0 Finished
    + CSS Style Changes for better Flexbox
    + Impressum and Datenschutzerklärung added
    + Google Map
    + Small css changes
    + beautify Code

--- lawyer-website/src/app/main/main.component.ts
+++ lawyer-website/src/app/main/main.component.ts
@@ -10,39 +10,15 @@ export class MainComponent implements OnInit {
 
   name = 'Klaus Löffler';
   job = 'Rechtsanwalt';
-  subHead = 'Ich bin der Anwalt deines Vertrauens';
-  introduction = 'Willkommen';
-  intro1 = 'Herzlich willkommen auf meiner Homepage. Mein Name ist Klaus Löffler und ich bin seit über 30 Jahren erfolgreicher Rechtsanwalt. Ich stehe meinen Mandanten in zahlreichen Rechtsgebieten mit Rat und Tat zur Seite. Neben ausführlicher und fundierter Rechtsberatung vertrete ich sie sowohl außergerichtlich als auch bei allen Gerichten.';
 
-  services = 'Leistungen';
-  serv1 = 'Strafrecht';
-  serv2 = 'Familienrecht';
-  serv3 = 'Zivilrecht';
-  serv4 = 'Andere';
-  serv5 = 'Dies das Recht';
-  serv1text = 'das thema ist super wichtig und ich bin voll gut daron';
-  serv2text = 'das thema ist super wichtig und ich bin voll gut daron';
-  serv3text = 'das thema ist super wichtig und ich bin voll gut daron';
-  serv4text = 'das thema ist super wichtig und ich bin voll gut daron';
-  serv5text = 'das thema ist super wichtig und ich bin voll gut daron';
-  
-  aboutme = 'Über mich';
-  aboutme1 = 'hdufgbnaougosahgfhsoifhioahfioebfhajskdhfkshfjd dddsdsd jkjlkajslkdjalksdjadsjlakljdakldjaklsdjladdhfjksguobuigbiuafbesuof';
-  aboutme2 = 'hdufgbnaougosahgfhsoifhioahfioebguobuigbiuafbesuof';
-  aboutme3 = 'hdufgbnaougosahgfhsoifhioahfioebguobuigbiuafbesuof';
-  aboutm4 = 'hdufgbnaougosahgfhsoifhioahfioebguobuigbiuafbesuof';
-
-  addressHead = 'Anschrift';
-  contactHead = 'Kontakt';
-  addressAddress = 'Hermanstraße 123';
-  addressPLZ = '12345';
-  addressCity = 'Hamburg';
+  addressStreet = 'Hermannstraße 14';
+  addressPLZCity = '20095 Hamburg';
   addressCountry = 'Deutschland';
 
-  number = '12345678';
-  fax = '987654321';
-  timesTitle = 'Sprechzeiten:';
+  number = '+4940/355323-0';
+  fax = '+4940/355323-23';
   times = 'Mittwoch 09:00-13:00';
+  mail = 'info@rechtsanwalthamburg.de';
 
   scroll(el) {
     el.scrollIntoView({behavior: "smooth"});
